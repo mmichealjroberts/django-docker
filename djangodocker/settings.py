a9156b179b347d012ef26dfd1cd7fec83cd66795
Michael Roberts <michael@winduplordvexxos.com>
2018-09-23 19:43:37 +0100

    Looks like this little if, else statement was giving a few people issues. DOCKER = True is certainly defined. Odd!

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -74,26 +74,15 @@ WSGI_APPLICATION = 'djangodocker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DOCKER:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'HOST': 'db',
-            'PORT': 5432,
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
-
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
